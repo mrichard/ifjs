364e1d47b68919604ba15a3bff59c1861f3e90be
Michel Richard <mrichard4@lm3000026417.home>
2015-03-15 21:52:00 -0400

    gitkeeps

--- ghost_config/config.js
+++ ghost_config/config.js
@@ -11,7 +11,16 @@ config = {
     // Configure your URL and mail settings here
     production: {
         url: 'http://my-ghost-blog.com',
-        mail: {},
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'Mailgun',
+                auth: {
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS
+                }
+            }
+        },
         database: {
             client: 'sqlite3',
             connection: {
